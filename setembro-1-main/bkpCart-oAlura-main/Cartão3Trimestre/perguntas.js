ac73d585bc68bddeae11b68ce4155496ef861ab4
brayanassis <142243436+brayanassis@users.noreply.github.com>
2024-10-29 11:00:57 -0300

    Add files via upload

---
+++ setembro-1-main/bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+//categoria,
+'Programação',
+//pergunta,
+'O que é JavaScript',
+//resposta,
+'O JavaScript é uma Linguagem de programação'
+)
+
+criaCartao(
+    //categoria,
+    'Programação',
+    //pergunta,
+    'O que é JavaScript',
+    //resposta,
+    'O JavaScript é uma Linguagem de programação'
+    )
+
+    criaCartao(
+        //categoria,
+        'Programação',
+        //pergunta,
+        'O que é JavaScript',
+        //resposta,
+        'O JavaScript é uma Linguagem de programação'
+        )
+
+criaCartao(
+'Literatura',
+'Qual é o nome da Arlen Fardos',
+'O nome e Pokémon',
+)        
